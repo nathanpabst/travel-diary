bd00a9ac35be4a9ec97ba2ef873d0effc51d63e4
Nathan Pabst <nathan.pabst@gmail.com>
2018-03-21 20:11:34 -0500

    adding destinations object

---
+++ main.js
@@ -0,0 +1,27 @@
+const destinations = [
+    {
+        location: "Switzerland"
+        image: "https://cloudfront.traillink.com/photos/fowler-trail_137668_sc.jpg",
+        description: "Switzerland trail",
+    },
+    {
+        location: "Tahiti",
+        image:"http://media.triseptsolutions.com/sites/vaxwebfjt/PublishingImages/FJT_images/Destinations/South-Pacific/Tahiti/Tahiti_1200x612_Hero.jpg",        
+        description: "Huts on the water, whoa",
+    },
+    {
+        location: "New Zealand",
+        image: "http://www.simonsholidays.in/wp-content/uploads/2015/04/international-holiday-of-New-Zealand.jpg",
+        description: "Mountains and water",
+    },
+    {
+        location: "Peru",
+        image:"http://famouswonders.com/wp-content/gallery/machu-picchu/machu-picchu-late-afternoon.jpg",        
+        description: "The famous Machu Picchu",
+    },
+    {
+        location: "China",
+        image: "https://robohash.org/inciduntsapientetenetur.png?size=100x100&set=set1",
+        description: "The Great Wall of China",
+    }
+];
