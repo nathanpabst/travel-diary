a21e2f8344db59da384617e29813fc76ebbb1dea
Nathan Pabst <nathan.pabst@gmail.com>
2018-03-26 21:26:57 -0500

    updating html css and js files

--- main.js
+++ main.js
@@ -38,39 +38,93 @@ const cardMaker = (destinationsArray) => {
         domString += `<h1>${destinations.location}</h1>`;
         domString += `<img class="image" src="${destinations.image}" alt="">`;
         domString += `<h3>${destinations.description}</h3>`;
-        domString += `<textarea class="input" placeholder="tell your story here" rows="4" columns="30"></textarea><br>`
+        domString += `<textarea class="input" placeholder="tell your story here" rows="4" columns="30"></textarea><br>`;
         domString += `<button class="card-button">Post</button>`;
         domString += `</div>`;
     });
     printToDom(domString, 'card-holder');
-}
+};
 cardMaker(destinations);
 
-// const diaryMaker = () => {
-//     let entry = '';
-//     diariesArray.forEach((entry) => {
-//         domString += `<div class="entry">`;
-//         domString += `<h1>${destinations.location}</h1>`;
-//         domString += `<textarea class="input" placeholder="tell your story here" rows="4" columns="30"></textarea><br>`
-//         domString += `</div>`;
-//     });
-//     printToDom(domString, 'diary-holder');
+const inputBox = document.getElementsByClassName('input');
+const switzerlandBtn = document.getElementById('switzerland');
+const tahitiBtn= document.getElementById('tahiti');
+const newZealandBtn = document.getElementById('new-zealand');
+const peruBtn= document.getElementById('peru');
+const chinaBtn= document.getElementById('china');
+const outputBox = document.getElementsByClassName('output');
 
-const addAllEventListeners = () => {
-    const allTheButtons = document.getElementsByClassName('card-button');
+switzerlandBtn.addEventListener('click', (e) => {
+    console.log("my event", e);
+    const userInput = inputBox.value;
+    let domOutput = switzerland[userInput];
+    outputBox.innerHTML = domOutput;
+})
 
+tahitiBtn.addEventListener('click', (e) => {
+    console.log("my event", e);
+    const userInput = inputBox.value;
+    let domOutput = tahiti[userInput];
+    outputBox.innerHTML = domOutput;
+})
+
+// newZealandBtn.addEventListener('click', (e) => {
+//     console.log("my event", e);
+//     const userInput = inputBox.value;
+//     let domOutput = new-zealand[userInput];
+//     outputBox.innerHTML = domOutput;
+// })
+
+peruBtn.addEventListener('click', (e) => {
+    console.log("my event", e);
+    const userInput = inputBox.value;
+    let domOutput = peru[userInput];
+    outputBox.innerHTML = domOutput;
+})
+
+chinaBtn.addEventListener('click', (e) => {
+    console.log("my event", e);
+    const userInput = inputBox.value;
+    let domOutput = china[userInput];
+    outputBox.innerHTML = domOutput;
+})
+
+const allTheButtons = document.getElementsByClassName('card-button');
     for (let i = 0; i < allTheButtons.length; i++) {
-        allTheButtons[i].addEventListener('click', moveToDiary); 
-    }
-};
-const moveToDiary = (e) => {
-    const nameOfDestination = e.target.parentNode.children[0];
-    nameOfDestination.classList.add('location');
-};
+        allTheButtons[i].addEventListener('click', (e) => {
+            const userInput = inputBox.value;
+            let domOutput = '';
+            if (e.target.id === 'switzerland') {
+                domOutput = switzerland[userInput];
+                } else if (e.target.id === 'tahiti') {
+                    domOutput = tahiti[userInput];
+                // } else if (e.target.id === 'new-zealand') { 
+                //     domOutput = new-zealand[userInput];
+                
+                } else if (e.target.id === 'peru') {
+                    domOutput = peru[userInput];
+                } else {
+                    domOutput = china[userInput];
+                }
+                outputBox.innerHTML = domOutput;
+            })
+            printToDom(domOutput, 'diary-holder');
+        }
+      
 
-const startApplication = () => {
-    buildDomString(destinations);
-    addAllEventListeners();
-};
+// once the Post button--within a particular card--is clicked, the input/text should move to the corresponding div class below
+// create one div for the locations--'diary-holder'
+// create an ID for each location
+// connect event-listeners targeting the appropriate child of the div
+// const moveIt = (e) => {
+//     const nameOfDestination = e.target.parentNode.children[0];
+//     nameOfDestination.classList.add('location');
+// };
 
-startApplication();
+
+// const startApplication = () => {
+//     cardMaker(destinations);
+//     addAllEventListeners();
+// };
+
+// startApplication();
