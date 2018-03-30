717753920eab6e29ba7b745356629a36cc276314
Nathan Pabst <nathan.pabst@gmail.com>
2018-03-29 20:17:29 -0500

    adding styling

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const destinations = [
     },
     {
         location: "Tahiti",
-        image:"http://media.triseptsolutions.com/sites/vaxwebfjt/PublishingImages/FJT_images/Destinations/South-Pacific/Tahiti/Tahiti_1200x612_Hero.jpg",        
+        image: "http://media.triseptsolutions.com/sites/vaxwebfjt/PublishingImages/FJT_images/Destinations/South-Pacific/Tahiti/Tahiti_1200x612_Hero.jpg",
         description: "Huts on the water, whoa",
     },
     {
@@ -16,7 +16,7 @@ const destinations = [
     },
     {
         location: "Peru",
-        image:"http://famouswonders.com/wp-content/gallery/machu-picchu/machu-picchu-late-afternoon.jpg",        
+        image: "http://famouswonders.com/wp-content/gallery/machu-picchu/machu-picchu-late-afternoon.jpg",
         description: "The famous Machu Picchu",
     },
     {
@@ -34,84 +34,45 @@ const printToDom = (domString, divId) => {
 const cardMaker = (destinationsArray) => {
     let domString = '';
     destinationsArray.forEach((destinations) => {
-        const destinationsId = {destinations.location.replace(' ', '_')}
+        const destinationsId = destinations.location.replace(' ', '_');
         domString += `<div class="card">`;
-        domString += `<h1>${destinations.location}</h1>`;
+        domString += `<h2>${destinations.location}</h2>`;
         domString += `<img class="image" src="${destinations.image}" alt="">`;
         domString += `<h3>${destinations.description}</h3>`;
         domString += `<textarea class="input" placeholder="tell your story here" rows="4" columns="30"></textarea><br>`;
-        domString += `<button class="card-button" id="${destinationsId}">Post</button>`;
+        domString += `<button class="card-button" id="destinationsId">Post</button>`;
         domString += `</div>`;
     });
     printToDom(domString, 'card-holder');
 };
 cardMaker(destinations);
 
-const inputBox = document.getElementsByClassName('input');
-const switzerlandBtn = document.getElementById('switzerland');
-const tahitiBtn= document.getElementById('tahiti');
-const newZealandBtn = document.getElementById('new-zealand');
-const peruBtn= document.getElementById('peru');
-const chinaBtn= document.getElementById('china');
-const outputBox = document.getElementsByClassName('output');
 
-switzerlandBtn.addEventListener('click', (e) => {
-    console.log("my event", e);
-    const userInput = inputBox.value;
-    let domOutput = switzerland[userInput];
-    outputBox.innerHTML = domOutput;
-})
 
-tahitiBtn.addEventListener('click', (e) => {
-    console.log("my event", e);
-    const userInput = inputBox.value;
-    let domOutput = tahiti[userInput];
-    outputBox.innerHTML = domOutput;
-})
+// grab submit buttons by class name and loop through to add event listeners to grab input data and move to output box below
+//look over the xhr-zoo code from Tuesday 3/27
 
-// newZealandBtn.addEventListener('click', (e) => {
-//     console.log("my event", e);
-//     const userInput = inputBox.value;
-//     let domOutput = new-zealand[userInput];
-//     outputBox.innerHTML = domOutput;
-// })
+const allTheButtons = document.getElementsByClassName('card-button');
+for (let i = 0; i < allTheButtons.length; i++) {
+    allTheButtons[i].addEventListener('click', (e) => {
+        const userInput = inputBox.value;
+        let domOutput = '';
+        if (e.target.id === 'switzerland') {
+            domOutput = switzerland[userInput];
+        } else if (e.target.id === 'tahiti') {
+            domOutput = tahiti[userInput];
+            // } else if (e.target.id === 'new-zealand') { 
+            //     domOutput = new-zealand[userInput];
 
-peruBtn.addEventListener('click', (e) => {
-    console.log("my event", e);
-    const userInput = inputBox.value;
-    let domOutput = peru[userInput];
-    outputBox.innerHTML = domOutput;
-})
+        } else if (e.target.id === 'peru') {
+            domOutput = peru[userInput];
+        } else {
+            domOutput = china[userInput];
+        }
+        printToDom(domOutput, 'diary-holder');
+    })
+}
 
-chinaBtn.addEventListener('click', (e) => {
-    console.log("my event", e);
-    const userInput = inputBox.value;
-    let domOutput = china[userInput];
-    outputBox.innerHTML = domOutput;
-})
-
-// const allTheButtons = document.getElementsByClassName('card-button');
-//     for (let i = 0; i < allTheButtons.length; i++) {
-//         allTheButtons[i].addEventListener('click', (e) => {
-//             const userInput = inputBox.value;
-//             let domOutput = '';
-//             if (e.target.id === 'switzerland') {
-//                 domOutput = switzerland[userInput];
-//                 } else if (e.target.id === 'tahiti') {
-//                     domOutput = tahiti[userInput];
-//                 // } else if (e.target.id === 'new-zealand') { 
-//                 //     domOutput = new-zealand[userInput];
-                
-//                 } else if (e.target.id === 'peru') {
-//                     domOutput = peru[userInput];
-//                 } else {
-//                     domOutput = china[userInput];
-//                 }
-//                 outputBox.innerHTML = domOutput;
-//             })
-//             printToDom(domOutput, 'diary-holder');
-//         }
-      
 
 // once the Post button--within a particular card--is clicked, the input/text should move to the corresponding div class below
 // create one div for the locations--'diary-holder'
